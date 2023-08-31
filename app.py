1753637a7051a15b5347fc297c23d4821e81557e
Octave Pouillot <octavepouillot0107@gmail.com>
2023-08-31 11:22:01 +0200

    first commit of API repo

---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Query, Response
+from typing import Annotated
+import mlflow
+from mlflow import MlflowClient
+import pandas as pd
+
+app = FastAPI()
+
+data = pd.read_csv('../df_test_cleaned.csv')
+
+# Load Model from mlflow
+tracking_URI = "http://127.0.0.1:5000"
+mlflow.set_tracking_uri(tracking_URI) 
+
+client = MlflowClient()
+rm = client.search_registered_models()
+if len(rm) != 0:
+    rm_name = rm[0].name
+    rm_run_id = rm[0].latest_versions[0].run_id
+else:
+    rm_run_id = ''
+    print('There is no model registered.')
+
+model_path = "runs:/" + rm_run_id + "/" + rm_name
+
+model = mlflow.sklearn.load_model(model_path)
+predictions = model.predict(data)
+probas = model.predict_proba(data)
+data["y_pred"] = predictions
+
+
+@app.get('/group/')
+def customers_stat(arr_features: Annotated[list[str] | None, Query()] = None):
+    sample_false = data[arr_features].loc[data["y_pred"] == 0].sample(1500)
+    sample_customers = pd.concat([sample_false, data[arr_features].loc[data["y_pred"] == 1].sample(1500)])
+    return Response(sample_customers.to_json(orient="records"), media_type="application/json")
+
+
+@app.get('/customer/')
+def predict_id(id: int):
+    prediction = int(predictions[id])
+    probability = probas[id].tolist()
+    infos = data.iloc[id, :]
+    dict_data = {'prediction': prediction,
+                 'probability': probability,
+                 'infos': infos}
+    return dict_data
