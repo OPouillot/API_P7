b307bf1169ed5b80e76a76d33d23ab7b174659b4
Octave Pouillot <octavepouillot0107@gmail.com>
2023-09-01 09:33:14 +0200

    try to change startup - install packages before gunicorn

--- startup.sh
+++ startup.sh
@@ -1,1 +1,5 @@
+RUN apt-get update && apt-get install -y --no-install-recommends apt-utils
+RUN apt-get -y install curl
+RUN apt-get install libgomp1
+
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
