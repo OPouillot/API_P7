a9449de2090d4852f858112842b0253dd1f6d7d2
Octave Pouillot <octavepouillot0107@gmail.com>
2023-09-02 08:54:49 +0200

    change startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-RUN apt-get update && apt-get install -y --no-install-recommends apt-utils
-RUN apt-get -y install curl
-RUN apt-get install libgomp1
+apt-get update && apt-get install -y --no-install-recommends apt-utils
+apt-get -y install curl
+apt-get install libgomp1
 
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
